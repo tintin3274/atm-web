b1f47cc52c00fb10d552f535ac7d100cfd67f4f7
tintin3274 <tintin3274@hotmail.co.th>
2020-09-27 22:31:37 +0700

    PUT to web service from website (=0=)

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -2,10 +2,7 @@ package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
@@ -28,11 +25,34 @@ public class BankAccountController {
     public String openAccount(@ModelAttribute BankAccount bankAccount, Model model) {
         bankAccountService.openBankAccount(bankAccount);
         model.addAttribute("bankaccounts",bankAccountService.getBankAccounts());
-        // **** Error ****
-        // @ModelAttribute Field error in object 'bankAccount' on field 'id': rejected value [null]
-        // now fixed temporary by bankaccount.html use hardcode id -1 "<input type="hidden" name="id" value="-1"/>"
-        //System.out.println(bankAccount.toString());
-        // ***************;
         return "redirect:bankaccount";
     }
+
+    @GetMapping("/edit/{id}")
+    public String getEditBankAccountPage(@PathVariable int id, Model model) {
+        BankAccount account = bankAccountService.getBankAccount(id);
+        model.addAttribute("bankAccount", account);
+        return "bankaccount-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editAccount(@PathVariable int id,
+                              @ModelAttribute BankAccount bankAccount,
+                              Model model) {
+
+        bankAccountService.editBankAccount(bankAccount);
+        model.addAttribute("bankaccounts",bankAccountService.getBankAccounts());
+        return "redirect:/bankaccount";
+    }
 }
+
+
+// **** Error ****
+// @ModelAttribute Field error in object 'bankAccount' on field 'id': rejected value [null]
+// now fixed temporary by use hardcode
+// bankaccount.html
+// <input type="hidden" name="id" value="-1"/>
+// bankaccount-edit.html
+// <input type="hidden" th:field="*{id}"/>
+// <input type="hidden" th:field="*{customerId}"/>
+// ***************;
